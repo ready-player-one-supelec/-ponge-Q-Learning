3a27e4938390d02f484a37d734f275ac4c9117ab
Kagamino <martin@lehoux.net>
2019-02-04 20:36:01 +0100

    use frontprop

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ import time
 import numpy as np
 import random
 
-from conf import render, iterations
+from conf import render, iterations, exploration_rate
 
 # Initialisation
 env = gym.make('Pong-v0')
 env.reset()
 if render: env.render()
+actions_values = [0, 2, 5]
 
 def init_game(env):
 #Initialisation du jeu : les 20 premières frames ne servent à rien
@@ -32,33 +33,44 @@ def to_state(observation):
             observationR[k-34][l]=observation[k][l][0]
     return np.array(observationR).reshape(1,len(observationR)*len(observationR[0]))[0]
 
-def point(env):
+def point(env, observation):
     reward = 0
     while reward == 0:
         # TODO: Compute action by Network
         observation, reward, done, info = env.step(env.action_space.sample())
+
+        actions = frontprop(observation)
+        if random.random() < exploration_rate:
+            observation, reward, done, info = env.step(env.action_space.sample())
+        else:
+            action = actions_values[actions.index(max(actions))]
+            observation, reward, done, info = env.step(action)
         if render:
             time.sleep(0.01)
             env.render()
-    return reward, done
+    return observation, reward, done
 
 
 def game(env, played):
+    env.reset()
+    observation, reward, done, info = env.step(env.action_space.sample())
     scores = [0, 0] # (ia, opponent)
     done = False
     while not done:
-        reward, done = point(env)
+        observation, reward, done = point(env, observation)
         played += 1
         if reward == 1:
             scores[0] += 1
         else:
             scores[1] += 1
         print("#{:5d}# IA - opp: {:2d} vs {:2d}".format(played, scores[0], scores[1]))
-    env.reset()
     print("End game")
     return scores, played
 
 # TEST
+def frontprop(observation):
+    return [0.1, 0.2, 0.4]
+
 init_game(env)
 played = 0
 while played < iterations:
